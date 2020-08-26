a4309210ebe86d0b743be6a5a3a435e14d83d2d1
J2mF <mp4-12cs5@hotmail.com>
2020-08-27 00:35:40 +0300

    Implemented Camera Follow

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+    [SerializeField] private Transform target;
+    private const float LERP_TIME = 10f;
+
+    private void LateUpdate() {
+        transform.position = Vector3.Lerp( transform.position, target.position, LERP_TIME );
+    }
+}
